e0e0e4a69d9fef90b9b89c30e7d9414b08dd4a70
Your Name <pmtuan98@gmail.com>
2020-03-13 16:55:18 +0700

    ex4solution

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -1,3 +1,13 @@
-Exercise 4:
-Write a JavaScript program to get the current date.
-Expected Output : mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+// Exercise 4:
+// Write a JavaScript program to get the current date.
+// Expected Output : mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+
+
+function getCurrentDate(){
+	var currentDate = new Date();
+	console.log('Current date: ');
+	return currentDate.getDate() + '/' + (currentDate.getMonth() + 1) + '/' + currentDate.getFullYear();
+}
+
+
+console.log(getCurrentDate());
